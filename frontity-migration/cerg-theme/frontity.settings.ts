e137e80374b3b306b47378ede6e5e6c7e46374ac
quinnouyang <quinn.ouy@gmail.com>
2022-05-08 22:36:16 -0500

    CPanel test deployment

---
+++ frontity-migration/cerg-theme/frontity.settings.ts
@@ -0,0 +1,61 @@
+import { Settings } from "frontity/types";
+import WpSource from "@frontity/wp-source/types";
+import Theme from "@frontity/mars-theme-typescript/types";
+
+const settings: Settings<Theme | WpSource> = {
+  "name": "cerg-theme",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme-typescript",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Nature",
+              "/category/nature/"
+            ],
+            [
+              "Travel",
+              "/category/travel/"
+            ],
+            [
+              "Japan",
+              "/tag/japan/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "url": "https://test.frontity.org"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
