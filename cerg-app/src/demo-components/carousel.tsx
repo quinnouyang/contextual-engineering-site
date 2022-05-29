64332e641d95fe1b7a8cecd207d279ec59e2e1c0
quinnouyang <quinn.ouy@gmail.com>
2022-05-28 21:17:54 -0700

    (Rough) Carousel

--- cerg-app/src/demo-components/carousel.tsx
+++ cerg-app/src/demo-components/carousel.tsx
@@ -2,138 +2,132 @@ import React from "react";
 import {
   Box,
   IconButton,
-  useBreakpointValue,
   Stack,
   Heading,
   Text,
   Container,
+  Link,
+  Show,
 } from "@chakra-ui/react";
-import { BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi";
 import Slider from "react-slick";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const settings = {
+const illiniBlueOpaque = "rgba(19, 41, 75, 0.5)";
+const illiniBlueOpaqueHover = "rgba(19, 41, 75, 0.75)";
+
+const sliderSettings = {
   dots: true,
-  arrows: false,
-  fade: true,
-  infinite: true,
   autoplay: true,
-  speed: 500,
-  autoplaySpeed: 5000,
-  slidesToShow: 1,
-  slidesToScroll: 1,
+  autoplaySpeed: 7000,
 };
 
+// UI Issue: Card hover still interactible if no link
+interface Card {
+  title: string;
+  text: string;
+  image: string;
+  link?: string;
+}
+
+const cards: Card[] = [
+  {
+    title: "Photo 1",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image:
+      "https://contextual.engineering.illinois.edu/wp-content/uploads/2018/06/spring-measurement-students.jpg",
+    link: "/team",
+  },
+  {
+    title:
+      "Extra long title to test how extra long titles look. Okay, maybe a bit longer...",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image:
+      "http://contextual.engineering.illinois.edu/wp-content/uploads/2018/01/KBM-animal-trough-2.jpg",
+    link: "/#",
+  },
+  {
+    title: "Photo 3",
+    text: "“Applying Societal Context to Technical Design Processes” workshop will be held at the 129th ASEE Annual Conference and Exposition in Minneapolis, MN, June 26. Come and learn about how to apply Contextual Engineering to your engineering courses and practices. Visit this website for more information.",
+    image:
+      "http://contextual.engineering.illinois.edu/wp-content/uploads/2018/01/IMG_0854.jpg",
+  },
+  // {
+  //   title: "Hahahaha",
+  //   text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //   image: "noimagesoit'swhite",
+  //   link: "/#",
+  // },
+];
+
 export default function Carousel() {
-  // As we have used custom buttons, we need a reference variable to
-  // change the state
   const [slider, setSlider] = React.useState<Slider | null>(null);
 
-  // These are the breakpoints which changes the position of the
-  // buttons as the screen size changes
-  const top = useBreakpointValue({ base: "90%", md: "50%" });
-  const side = useBreakpointValue({ base: "30%", md: "40px" });
-
-  // This list contains all the data for carousels
-  // This can be static or loaded from a server
-  const cards = [
-    {
-      title: "Photo 1",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image:
-        "https://contextual.engineering.illinois.edu/wp-content/uploads/2018/06/spring-measurement-students.jpg",
-    },
-    {
-      title: "Photo 2",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image:
-        "http://contextual.engineering.illinois.edu/wp-content/uploads/2018/01/KBM-animal-trough-2.jpg",
-    },
-    {
-      title: "Photo 3",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      image:
-        "http://contextual.engineering.illinois.edu/wp-content/uploads/2018/01/IMG_0854.jpg",
-    },
-  ];
-
   return (
-    <Box
-      position={"relative"}
-      height={"600px"}
-      width={"full"}
-      overflow={"hidden"}
-    >
-      {/* CSS files for react-slick */}
-      <link
-        rel="stylesheet"
-        type="text/css"
-        charSet="UTF-8"
-        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-      />
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-      />
-      {/* Left Icon */}
-      <IconButton
-        aria-label="left-arrow"
-        variant="ghost"
-        position="absolute"
-        left={side}
-        top={top}
-        transform={"translate(0%, -50%)"}
-        color="white"
-        zIndex={2}
-        onClick={() => slider?.slickPrev()}
-      >
-        <BiLeftArrowAlt size="40px" />
-      </IconButton>
-      {/* Right Icon */}
-      <IconButton
-        aria-label="right-arrow"
-        variant="ghost"
-        position="absolute"
-        right={side}
-        top={top}
-        transform={"translate(0%, -50%)"}
-        color="white"
-        zIndex={2}
-        onClick={() => slider?.slickNext()}
-      >
-        <BiRightArrowAlt size="40px" />
-      </IconButton>
-      {/* Slider */}
-      <Slider {...settings} ref={(slider: any) => setSlider(slider)}>
+    <Box position={"relative"}>
+      <Show above="md">
+        <IconButton
+          aria-label="left-arrow"
+          position="absolute"
+          left={"2em"}
+          top={"50%"}
+          color="cloudWhite.50"
+          background={illiniBlueOpaque}
+          _hover={{ background: illiniBlueOpaqueHover }}
+          zIndex={2}
+          onClick={() => slider?.slickPrev()}
+        >
+          <ChevronLeftIcon boxSize="2em" />
+        </IconButton>
+        <IconButton
+          aria-label="right-arrow"
+          position="absolute"
+          right={"2em"}
+          top={"50%"}
+          color="cloudWhite.50"
+          background={illiniBlueOpaque}
+          _hover={{ background: illiniBlueOpaqueHover }}
+          zIndex={2}
+          onClick={() => slider?.slickNext()}
+        >
+          <ChevronRightIcon boxSize="2em" />
+        </IconButton>
+      </Show>
+
+      <Slider {...sliderSettings} ref={(slider: any) => setSlider(slider)}>
         {cards.map((card, index) => (
+          // Could refactor (redundant nested Stacks because I forgot how to center)
           <Box
             key={index}
-            height={"6xl"}
-            position="relative"
-            backgroundPosition="center"
-            backgroundRepeat="no-repeat"
+            h="50vh"
             backgroundSize="cover"
-            backgroundImage={`url(${card.image})`}
+            backgroundPosition={"center"}
+            backgroundImage={card.image}
           >
-            {/* This is the block you need to change, to customize the caption */}
-            <Container size="container.lg" height="600px" position="relative">
-              <Stack
-                spacing={6}
-                w={"full"}
-                maxW={"lg"}
-                position="absolute"
-                top="50%"
-                transform="translate(0, -50%)"
-              >
-                <Heading
-                  fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}
-                  color={"white"}
+            <Container h="full">
+              <Stack h="full" justifyContent={"center"}>
+                <Stack
+                  as={Link}
+                  href={card.link}
+                  background={illiniBlueOpaque}
+                  p="1em"
+                  maxW="60ch"
+                  _hover={{ background: illiniBlueOpaqueHover }}
                 >
-                  {card.title}
-                </Heading>
-                <Text fontSize={{ base: "md", lg: "lg" }} color="white">
-                  {card.text}
-                </Text>
+                  <Heading
+                    fontSize={{ base: "2xl", md: "3xl", lg: "4xl" }}
+                    color="cloudWhite.50"
+                  >
+                    {card.title}
+                  </Heading>
+                  <Text
+                    fontSize={{ base: "sm", md: "md", lg: "lg" }}
+                    color="cloudWhite.50"
+                  >
+                    {card.text}
+                  </Text>
+                </Stack>
               </Stack>
             </Container>
           </Box>
