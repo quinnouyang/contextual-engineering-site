3065b551aea57303ca95ab4f05f1441b2ccfc2e0
quinnouyang <quinn.ouy@gmail.com>
2022-05-30 18:45:23 -0700

    Custom server for CPanel

--- cerg-app/server.js
+++ cerg-app/server.js
@@ -1,37 +1,28 @@
-// server.js
 const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
-
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
-// when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port });
+
+const port = !dev ? process.env.PORT : 3000;
+
+// Create the Express-Next App
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
+app
+  .prepare()
+  .then(() => {
+    createServer((req, res) => {
       const parsedUrl = parse(req.url, true);
       const { pathname, query } = parsedUrl;
-
-      if (pathname === "/a") {
-        await app.render(req, res, "/a", query);
-      } else if (pathname === "/b") {
-        await app.render(req, res, "/b", query);
-      } else {
-        await handle(req, res, parsedUrl);
-      }
-    } catch (err) {
-      console.error("Error occurred handling", req.url, err);
-      res.statusCode = 500;
-      res.end("internal server error");
-    }
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
+      handle(req, res, parsedUrl);
+      console.log("pathname", pathname);
+    }).listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
   });
-});
