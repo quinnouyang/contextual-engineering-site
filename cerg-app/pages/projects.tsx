95da099b671b6ce83a8998767c92d0a6e9abde1b
quinnouyang <quinn.ouy@gmail.com>
2022-06-16 00:30:00 -0700

    Other: Hero organization, BioCard tweaks, etc.

--- cerg-app/pages/projects.tsx
+++ cerg-app/pages/projects.tsx
@@ -1,6 +1,6 @@
 import { Container, Box, SimpleGrid, Center } from "@chakra-ui/react";
 import Footer from "../src/components/Footer";
-import Hero from "../src/components/Hero";
+import Hero from "../src/components/Heros/ColorHero";
 import NavBar from "../src/components/NavBar/NavBar";
 import ProjectCard from "../src/components/ProjectCard";
 import { projects } from "../src/types/projects";
@@ -9,7 +9,7 @@ export default function ProjectsPage() {
   return (
     <Box bg="lightGrey">
       <NavBar {...{ label: "Research", childLabel: "Projects" }} />
-      <Hero text="food" />
+      <Hero {...{ text: "aaa" }} />
       <Container maxW="container.xl" p="1em">
         <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing="1em">
           {projects.map((project) => {
--- cerg-app/src/components/Heros/ColorHero.tsx
+++ cerg-app/src/components/Heros/ColorHero.tsx
@@ -1,12 +1,17 @@
 import { VStack, Container, Divider, Heading, Text } from "@chakra-ui/react";
 
-export default function Hero(text: string, color?: string) {
+export type ColorHeroProps = {
+  readonly text: string;
+  readonly color?: string;
+};
+
+export default function ColorHero({ text, color }: ColorHeroProps) {
   return (
     <VStack
       py={{ base: "2em", md: "3em", lg: "4em" }}
       h="full"
       // Covers NavBar boxShadow?
-      bgColor="heritageOrange.100"
+      bgColor={color ?? "heritageOrange.100"}
     >
       <Container justifyContent="center" centerContent>
         <Heading
@@ -18,13 +23,14 @@ export default function Hero(text: string, color?: string) {
         </Heading>
         <Divider m="1em" borderColor="cloudWhite.50" />
         <Text color="cloudWhite.50" fontSize={{ base: "14px", sm: "1em" }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          {text ??
+            `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          culpa qui officia deserunt mollit anim id est laborum.`}
         </Text>
       </Container>
     </VStack>
