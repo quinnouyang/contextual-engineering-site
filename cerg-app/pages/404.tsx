53dc187e113e157384d4a6b33a3a42f7e046b564
quinnouyang <quinn.ouy@gmail.com>
2022-06-21 23:59:25 -0700

    Custom error page

--- cerg-app/pages/404.tsx
+++ cerg-app/pages/404.tsx
@@ -1,11 +1,23 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { Button, Container, Heading, Link, Text } from "@chakra-ui/react";
 import PageWrapper from "../src/components/PageWrapper";
 
 export default function ErrorPage() {
   return (
     <PageWrapper>
-      <Container maxW="container.lg" centerContent>
-        <Heading m="1em">u in wrong place</Heading>
+      <Container maxW="container.xl" centerContent>
+        <Heading m="1em">
+          either u got a wrong/outdated link or i broke something:(
+        </Heading>
+        <Button
+          as={Link}
+          href="/"
+          bgColor="illiniOrange"
+          color="white"
+          _hover={{ color: "white", bgColor: "illiniBlue" }}
+        >
+          Take me home!
+        </Button>
+        <Text py="1em">(watch this be broken too lol)</Text>
       </Container>
     </PageWrapper>
   );
