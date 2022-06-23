09a86fea4c31d35aa1d62c4250397b51a2be9027
quinnouyang <quinn.ouy@gmail.com>
2022-06-23 16:10:30 -0700

    Added bundle analyzer

--- cerg-app/next.config.js
+++ cerg-app/next.config.js
@@ -1,6 +1,10 @@
+const withPlugins = require("next-compose-plugins");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins([[withBundleAnalyzer], nextConfig]);
