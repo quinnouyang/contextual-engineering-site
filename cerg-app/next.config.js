11b9d8c0e4578b189f855bbb8e882c0082c6432a
quinnouyang <quinn.ouy@gmail.com>
2022-06-25 20:38:40 -0700

    Image optimization configurations

--- cerg-app/next.config.js
+++ cerg-app/next.config.js
@@ -2,9 +2,18 @@ const withPlugins = require("next-compose-plugins");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
+const withOptimizedImages = require("next-optimized-images");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    disableStaticImages: true,
+  },
 };
 
-module.exports = withPlugins([[withBundleAnalyzer], nextConfig]);
+module.exports = withPlugins([
+  [withBundleAnalyzer],
+  // [withOptimizedImages],
+  nextConfig,
+]);
