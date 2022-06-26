b4ece2831194840d2dccb986681e1a1581622ced
quinnouyang <quinn.ouy@gmail.com>
2022-06-25 17:53:44 -0700

    Courses & Textbook layout tweaks

--- cerg-app/pages/courses-textbook.tsx
+++ cerg-app/pages/courses-textbook.tsx
@@ -1,98 +1,196 @@
 import PageWrapper from "../src/components/PageWrapper";
-import { Container, VStack, Box, Text, Link, Image, Center } from "@chakra-ui/react";
-import ColorHero from "../src/components/Heros/ColorHero";
+import {
+  Container,
+  VStack,
+  Text,
+  Link,
+  Image,
+  Center,
+  Heading,
+  Divider,
+  Flex,
+  Button,
+} from "@chakra-ui/react";
+import ColorHero, { ColorHeroProps } from "../src/components/Heros/ColorHero";
+import React from "react";
 
-function InfoBox(props: any) {
-  return <Text border="2px solid black" margin="10px" padding="5px">{ props.children }</Text>
-}
+const coursesHeroProps: ColorHeroProps = {
+  heading: "Courses",
+  text: (
+    <Text
+      color="white"
+      fontSize={{ base: "md", md: "lg", lg: "xl" }}
+      fontWeight="medium"
+    >
+      A comprehensive list of UIUC courses on or related to contextual design
+      and engineering, open to undergraduate and graduate students. Many of
+      these are taught by our principal investigator,{" "}
+      <Link
+        href="https://appliedresearch.illinois.edu/directory/profile/awitmer"
+        variant="underline"
+        color="white"
+        isExternal
+      >
+        Ann Witmer
+      </Link>
+      .
+    </Text>
+  ),
+  bgColor: "illiniBlue",
+};
 
-const SPACING = "10px";
+const textbookHeroProps: ColorHeroProps = {
+  heading: "Textbook",
+  text: "Coming this summer: the first comprehensive text dedicated towards contexutal engineering!",
+};
 
 export default function CoursesTextbookPage() {
   return (
     <PageWrapper
       {...{ mainTitle: "Courses & Textbook", category: "Resources" }}
     >
-      <ColorHero heading="Courses"></ColorHero>
-      <VStack marginLeft="10%" marginRight="10%" marginBottom={SPACING}>
-        {
-          COURSES.map(course =>
-            <Box backgroundColor="lightgray" width="100%" margin={SPACING}>
-              <Text margin={SPACING}><b>{ course.title }</b></Text>
-              <InfoBox>{ course.description }</InfoBox>
-              {
-                course.notices === undefined ? '' : course.notices.map(notice => <InfoBox>{ notice }</InfoBox>)
-              }
-            </Box>
-          )
-        }
-      </VStack>
-      <ColorHero heading="Textbook"></ColorHero>
-      <Container marginTop={SPACING} marginBottom={SPACING}>
-          <VStack spacing={SPACING}>
-            <Center><Text fontSize="3xl">{ TEXTBOOK_TITLE }</Text></Center>
-            <Text>{ TEXTBOOK_DESCRIPTION }</Text>
-            <Link href={TEXTBOOK_URL} isExternal>
-              <Center>
-                <Image width="50%" height="50%" src={TEXTBOOK_IMAGE} />
-              </Center>
-            </Link>
-            <Center>
-                <Link href={ TEXTBOOK_URL } fontSize="2xl" color="blue" isExternal>Go to book</Link>
-            </Center>
+      <ColorHero {...coursesHeroProps} />
+      <Container py={["1em", "2em", "3em"]}>
+        <VStack spacing={["1em", "1.5em"]}>
+          {COURSES.map(({ title, subtitle, description, link }) => (
+            <VStack p={["1.5em", "2em"]} align="left" bgColor="white">
+              <Heading
+                as={link ? Link : undefined}
+                href={link}
+                textDecoration={link ? "underline" : undefined}
+                _hover={{ textDecoration: link ? "underline" : "none" }}
+                fontSize={["md", "xl", "2xl"]}
+              >
+                {title}
+              </Heading>
+              <Heading fontSize={["md", "xl"]}>{subtitle}</Heading>
+              <Divider />
+              {typeof description === "string" ? (
+                <Text variant="article">{description}</Text>
+              ) : (
+                description
+              )}
+            </VStack>
+          ))}
+        </VStack>
+      </Container>
+      <ColorHero {...textbookHeroProps} />
+      <Container py={["1em", "2em", "3em"]}>
+        <Flex
+          direction={{ base: "column-reverse", lg: "row" }}
+          p={["1.5em", "2em"]}
+          bgColor="white"
+          gap={["1.5em", "2em"]}
+        >
+          <VStack spacing="1em">
+            <Heading fontWeight="semibold">{textbook.title}</Heading>
+            <Text variant="article">{textbook.description}</Text>
+            <Button
+              as={Link}
+              href={textbook.link}
+              isExternal
+              size="lg"
+              textColor="white"
+              bg="illiniOrange"
+              _hover={{ color: "white", bg: "illiniBlue" }}
+            >
+              See it on Amazon
+            </Button>
           </VStack>
+          <Image
+            src="/images/textbook-cover.jpeg"
+            w="full"
+            maxW="20em"
+            alignSelf="center"
+            alt="Textbook cover"
+          />
+        </Flex>
       </Container>
     </PageWrapper>
   );
 }
 
-enum Semester {
-  Fall,
-  Spring
-}
-
-interface Course {
-  semester: Semester;
+type Course = {
   title: string;
-  description: string;
-  notices?: any[];
-}
+  subtitle: string;
+  description: string | React.ReactNode;
+  link?: string;
+};
 
-export const COURSES: Course[] = [
+const COURSES: Course[] = [
   {
-      semester: Semester.Fall,
-      title: 'ABE 232/AFST 233 – Context in International Interventions',
-      description: 'An exploration of the progress and pitfalls of a real-life engineering project in Nigeria to make the case for Contextual Engineering, which evaluates non-technical influences to determine the likelihood that an infrastructure intervention will work for a particular international community. While engineering principles will be included in this course, non-engineers are encouraged to enroll and learn about the relationship between technical and social sciences when working on an international project in Sub-Saharan Africa and beyond. 3 credits.',
-      notices: [<b>GEN ED COURSE IN NON-WESTERN CULTURE STUDIES AND SOCIAL SCIENCES!</b>]
+    title: "ABE 232/AFST 233 - Context in International Interventions",
+    subtitle: "Term: Fall 2022. Credits: 3 hours",
+    description:
+      "An exploration of the progress and pitfalls of a real-life engineering project in Nigeria to make the case for Contextual Engineering, which evaluates non-technical influences to determine the likelihood that an infrastructure intervention will work for a particular international community. While this course includes engineering principles, we encourage non-engineers to enroll and learn about the relationship between technical and social sciences when working on international projects in Sub-Saharan Africa and beyond. Counts towards the non-western culture and social sciences general education categories.",
+    link: "https://courses.illinois.edu/schedule/2022/fall/ABE/232",
   },
   {
-      semester: Semester.Fall,
-      title: 'ABE 532 – Advanced Context in International Interventions',
-      description: 'Contextual Engineering addresses the loss of infrastructure usability, sustainability, and resiliency in non-industrialized societal settings that often results from disconnects and differing objectives among stakeholders. Using case studies and technical infrastructure designs, the impacts of globalization, Western attitudes, power dynamics, and place-based knowledge are explored and applied to engineering design and implementation processes. Application of these concepts will then be conducted for specific design efforts associated with the student’s own discipline. 4 credits.'
+    title: "ABE 532 - Advanced Context in International Interventions",
+    subtitle: "Term: Fall 2022. Credits: 4 hours",
+    description:
+      "Sharing a lecture with ABE 232/AFST 233, this advanced course on Contextual Engineering addresses the loss of infrastructure usability, sustainability, and resiliency in non-industrialized societal settings that often results from disconnects and differing objectives among stakeholders. Using case studies and technical infrastructure designs, the impacts of globalization, Western attitudes, power dynamics, and place-based knowledge are explored and applied to engineering design and implementation processes. Application of these concepts will then be conducted for specific design efforts associated with the student's own discipline.",
+    link: "https://courses.illinois.edu/schedule/2022/fall/ABE/532",
   },
   {
-      semester: Semester.Fall,
-      title: 'ABE 450/LAST440-550 – International Water Project I',
-      description: 'First part of two-semester, applied design course provides assistance for a rural Central American community in designing and implementing a contextually appropriate water system. Enrollees will work closely with alumni mentors and professional advisors on client assessment, conceptual engineering design, and strategies for implementation, and have the opportunity to travel to Honduras during Winter Break. Undergrad-3 credits; Grad-4 credits.',
-      notices: [
-          'Returning for 2022-23 AY!!',
-          <>Website: <Link href="http://hwpillinois.weebly.com/" color="blue" isExternal>http://hwpillinois.weebly.com/</Link></>
-      ]
+    title:
+      "ABE 199 - International Engineering Service Project Management for Student Organizations",
+    subtitle: "Term: Fall 2022. Credit: 1 hour (undergraduate)",
+    description:
+      "When students are drawn to participate in engineering service organizations, many new members quickly drop out in frustration from the complexity of technical and logistical challenges associated with working with an unfamiliar client. This course trains student practitioners on how to assess, design, and implement a project by addressing the technical design, societal conditions, and project management skills that are needed to allow participants to contribute meaningfully to their projects.",
+    link: "https://courses.illinois.edu/schedule/2022/fall/ABE/199",
   },
   {
-      semester: Semester.Spring,
-      title: 'ABE 199 – International Engineering Service Project Management for Student Organizations',
-      description: 'Students are drawn to participate in engineering service organizations for a variety of reasons, but with little preparation and understanding of the technical and logistical challenges associated with working with an unfamiliar client, many new members quickly drop out in frustration. This course trains student practitioners on how to assess, design, and implement a project by addressing the technical design, societal conditions, and project management skills that are needed to allow participants to contribute meaningfully to their projects. Undergrad-1 credit.',
+    title: "ABE 450/LAST440-550 - International Water Project I",
+    subtitle:
+      "Term: Fall 2022. Credits: 3 hours (undergraduate), 4 hours (graduate)",
+    description: (
+      <Text variant="article">
+        The first part of two-semester applied design course providing
+        assistance for a rural Central American community in designing and
+        implementing a contextually appropriate water system. Enrollees will
+        work closely with alumni mentors and professional advisors on client
+        assessment, conceptual engineering design, and strategies for
+        implementation, and have the opportunity to travel to Honduras during
+        Winter Break. Check out the project website{" "}
+        <Link
+          href="http://hwpillinois.weebly.com/"
+          variant="underline"
+          isExternal
+        >
+          here!
+        </Link>
+      </Text>
+    ),
+    link: "https://courses.illinois.edu/schedule/2022/fall/ABE/450",
   },
   {
-      semester: Semester.Spring,
-      title: 'ABE 451/LAST441-550 – International Water Project II ',
-      description: 'Second part of two-semester, applied design course provides assistance for a rural Central American community in designing and implementing a contextually appropriate water system. Enrollees will work closely with alumni mentors and professional advisors on completing final design of a functional sanitary water system, revising preliminary designs based on data gathered during travel to Honduras during Winter Break. Undergrad-3 credits; Grad-4 credits.',
-      notices: [<>Website: <Link href="http://hwpillinois.weebly.com/" color="blue" isExternal>http://hwpillinois.weebly.com/</Link></>]
-  }
+    title: "ABE 451/LAST441-550 - International Water Project II ",
+    subtitle:
+      "Term: Spring 2023. Credits: 3 hours (undergraduate), 4 hours (graduate)",
+    description: (
+      <Text variant="article">
+        The second part of two-semester applied design course providing
+        assistance for a rural Central American community in designing and
+        implementing a contextually appropriate water system. Enrollees will
+        work closely with alumni mentors and professional advisors on client
+        assessment, conceptual engineering design, and strategies for
+        implementation, and have the opportunity to travel to Honduras during
+        Winter Break. Check out the project website{" "}
+        <Link
+          href="http://hwpillinois.weebly.com/"
+          variant="underline"
+          isExternal
+        >
+          here!
+        </Link>
+      </Text>
+    ),
+  },
 ];
 
-const TEXTBOOK_TITLE: string = 'Predicting Rural Responses to Climate Change';
-const TEXTBOOK_DESCRIPTION: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. (50 words)';
-const TEXTBOOK_IMAGE: string = '/images/gabions1.jpeg';
-const TEXTBOOK_URL: string = 'https://www.google.com/';
+const textbook = {
+  title: "Contextual Engineering: Translating User Voice Into Design",
+  description: `"This book shows readers a new way of thinking about the engineering design process, as well as how to expand their understanding of the role of technical designers in society, whether working with international communities or user populations from their own hometown...At the same time, readers will learn techniques to explore their own predispositions and the biases they may not be aware they have, equipping them to interact with others more impartially. This self-reflection process also assists the designer in working with and accepting the uncertainty that is inherent in exploring context."`,
+  link: "https://www.amazon.com/Contextual-Engineering-Translating-Synthesis-Technology/dp/3031076915",
+};
