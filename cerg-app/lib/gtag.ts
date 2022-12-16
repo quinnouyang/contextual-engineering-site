0fb1ebef56b19c25cfe2b82e2cf3023a385d783b
quinnouyang <quinn.ouy@gmail.com>
2022-12-15 18:51:51 -0600

    Refreshed dependencies, pruned custom server and other unused code

--- cerg-app/lib/gtag.ts
+++ cerg-app/lib/gtag.ts
@@ -1,25 +1,25 @@
-export const GA_TRACKING_ID = "G-E0DZ1M3H2X";
+export const GA_TRACKING_ID = null;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
-  if (typeof window !== "undefined")
-    window.gtag("config", GA_TRACKING_ID, {
-      page_path: url,
-    });
+	if (typeof window !== "undefined")
+		window.gtag("config", GA_TRACKING_ID, {
+			page_path: url,
+		});
 };
 
 type GTagEvent = {
-  action: string;
-  category: string;
-  label: string;
-  value: number;
+	action: string;
+	category: string;
+	label: string;
+	value: number;
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent) => {
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value,
-  });
+	window.gtag("event", action, {
+		event_category: category,
+		event_label: label,
+		value,
+	});
 };
