071f25090601fce9720736e79b42cb2b8b4a8741
quinnouyang <quinn.ouy@gmail.com>
2022-12-29 17:50:32 -0800

    Commented out Google Analytics (was not functioning)

--- cerg-app/pages/_app.tsx
+++ cerg-app/pages/_app.tsx
@@ -6,20 +6,20 @@ import "@fontsource/montserrat/600.css"; // Imperfect font imports (not all font
 import "@fontsource/montserrat/500.css";
 import "@fontsource/montserrat/200.css";
 import "@fontsource/montserrat";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import * as gtag from "../lib/gtag";
+// import { useRouter } from "next/router";
+// import { useEffect } from "react";
+// import * as gtag from "../lib/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => gtag.pageview(url);
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+  // useEffect(() => {
+  //   const handleRouteChange = (url: URL) => gtag.pageview(url);
+  //   router.events.on("routeChangeComplete", handleRouteChange);
+  //   return () => {
+  //     router.events.off("routeChangeComplete", handleRouteChange);
+  //   };
+  // }, [router.events]);
 
   return (
     <ChakraProvider theme={CustomTheme}>
