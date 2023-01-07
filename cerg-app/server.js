89b8d2e0e64c2e9670b6316ecbe4cf4599cc61ce
quinnouyang <quinn.ouy@gmail.com>
2023-01-07 15:24:07 -0800

    Re-enable cerg-app for SSR

---
+++ cerg-app/server.js
@@ -0,0 +1,118 @@
+// server.js
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+	createServer(async (req, res) => {
+		try {
+			// Be sure to pass `true` as the second argument to `url.parse`.
+			// This tells it to parse the query portion of the URL.
+			const parsedUrl = parse(req.url, true);
+			const { pathname, query } = parsedUrl;
+
+			if (pathname === "/a") {
+				await app.render(req, res, "/a", query);
+			} else if (pathname === "/b") {
+				await app.render(req, res, "/b", query);
+			} else {
+				await handle(req, res, parsedUrl);
+			}
+		} catch (err) {
+			console.error("Error occurred handling", req.url, err);
+			res.statusCode = 500;
+			res.end("internal server error");
+		}
+	}).listen(port, (err) => {
+		if (err) throw err;
+		console.log(`> Ready on http://${hostname}:${port}`);
+	});
+});
+
+// const express = require("express");
+// const next = require("next");
+// const LRUCache = require("lru-cache");
+
+// const port = parseInt(process.env.PORT, 10) || 3000;
+// const dev = process.env.NODE_ENV !== "production";
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
+
+// // This is where we cache our rendered HTML pages
+// const ssrCache = new LRUCache({
+//   max:
+//     100 *
+//     1024 *
+//     1024 /* cache size will be 100 MB using `return n.length` as length() function */,
+//   length: function (n, key) {
+//     return n.length;
+//   },
+//   maxAge: 1000 * 60 * 60 * 24 * 30,
+// });
+
+// app.prepare().then(() => {
+//   const server = express();
+
+//   server.get("/_next/*", (req, res) => {
+//     /* serving _next static content using next.js handler */
+//     handle(req, res);
+//   });
+
+//   server.get("*", (req, res) => {
+//     /* serving page */
+//     return renderAndCache(req, res);
+//   });
+
+//   /* starting server */
+//   server.listen(port, (err) => {
+//     if (err) throw err;
+//     console.log(`> Ready on http://localhost:${port}`);
+//   });
+// });
+
+// /*
+//  * NB: make sure to modify this to take into account anything that should trigger
+//  * an immediate page change (e.g a locale stored in req.session)
+//  */
+// function getCacheKey(req) {
+//   return `${req.path}`;
+// }
+
+// async function renderAndCache(req, res) {
+//   const key = getCacheKey(req);
+
+//   // If we have a page in the cache, let's serve it
+//   if (ssrCache.has(key)) {
+//     //console.log(`serving from cache ${key}`);
+//     res.setHeader("x-cache", "HIT");
+//     res.send(ssrCache.get(key));
+//     return;
+//   }
+
+//   try {
+//     //console.log(`key ${key} not found, rendering`);
+//     // If not let's render the page into HTML
+//     const html = await app.renderToHTML(req, res, req.path, req.query);
+
+//     // Something is wrong with the request, let's skip the cache
+//     if (res.statusCode !== 200) {
+//       res.send(html);
+//       return;
+//     }
+
+//     // Let's cache this page
+//     ssrCache.set(key, html);
+
+//     res.setHeader("x-cache", "MISS");
+//     res.send(html);
+//   } catch (err) {
+//     app.renderError(err, req, res, req.path, req.query);
+//   }
+// }
