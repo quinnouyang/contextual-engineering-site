fea404909242aad067dddf6d2c9bba63254f95d3
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2023-01-09 20:26:29 -0800

    trunk: Removed unused code / comments

--- trunk/src/components/CaptionedImage.tsx
+++ trunk/src/components/CaptionedImage.tsx
@@ -1,26 +1,26 @@
-import { Image, VStack, Text, AspectRatio } from "@chakra-ui/react";
+import { VStack, Text } from "@chakra-ui/react";
 
 export type CaptionedImageProps = {
-  readonly image: React.ReactNode;
-  readonly caption: string | React.ReactNode;
-  readonly whiteBg?: boolean;
+	readonly image: React.ReactNode; // Weak typing
+	readonly caption: string | React.ReactNode;
+	readonly whiteBg?: boolean;
 };
 
 export default function CaptionedImage({
-  image,
-  caption,
-  whiteBg,
+	image,
+	caption,
+	whiteBg,
 }: CaptionedImageProps) {
-  return (
-    <VStack
-      align="center"
-      w="fit-content"
-      bgColor={whiteBg === false ? "none" : "white"}
-    >
-      {image}
-      <Text px="2em" fontSize={["xs", "sm", "md"]} display={["none", "flex"]}>
-        {caption}
-      </Text>
-    </VStack>
-  );
+	return (
+		<VStack
+			align="center"
+			w="fit-content"
+			bgColor={whiteBg === false ? "none" : "white"}
+		>
+			{image}
+			<Text px="2em" fontSize={["xs", "sm", "md"]} display={["none", "flex"]}>
+				{caption}
+			</Text>
+		</VStack>
+	);
 }
