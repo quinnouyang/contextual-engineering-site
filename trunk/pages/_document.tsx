d394a08afa3069a63fa97785d8faf5c78a6a341d
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2023-01-22 20:48:58 -0600

    trunk: Removed unused code, mainly Google Analytics support

--- trunk/pages/_document.tsx
+++ trunk/pages/_document.tsx
@@ -1,31 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-// import Script from "next/script";
-// import { GA_TRACKING_ID } from "../lib/gtag";
 
 export default class CustomDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          {/* <Script
-            id="Google Analytics Script"
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <Script
-            id="Google Analytics Tag"
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          /> */}
           <link
             rel="apple-touch-icon"
             sizes="180x180"
